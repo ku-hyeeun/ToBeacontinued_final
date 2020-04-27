9b3b5a2e1a09c585558f57c65ec9ec6551f73bcf
JeongYeonseo225 <wjddustj2350@gmail.com>
2020-04-27 19:17:19 +0900

    Second Commit

--- app/src/main/java/com/androidapp/tobeacontinue/MenuActivity.java
+++ app/src/main/java/com/androidapp/tobeacontinue/MenuActivity.java
@@ -1,14 +1,87 @@
 package com.androidapp.tobeacontinue;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.Toast;
 
-public class MenuActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MenuActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,FragmentCallback {
+
+    Fragment1 fragment1;
+
+    DrawerLayout drawer;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        toolbar = findViewById(R.id.toolbar);
+          setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        fragment1 = new Fragment1();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container,fragment1).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id==R.id.menu1){
+            Toast.makeText(this,"첫 번째 메뉴 선택됨", Toast.LENGTH_LONG).show();
+            OnFragmentSelected(0, null);
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void OnFragmentSelected(int position, Bundle bundle) {
+        Fragment curFragment = null;
+
+        if(position == 0){
+            curFragment = fragment1;
+            toolbar.setTitle("첫 번째 화면");
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, curFragment).commit();
+
     }
 }
