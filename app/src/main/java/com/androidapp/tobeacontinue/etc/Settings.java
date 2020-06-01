bbc7b5c01f0b61474c6f48a4233516c3f1abae09
JeongYeonseo225 <wjddustj2350@gmail.com>
2020-06-02 03:13:39 +0900

    12th commit - 설정창 바꿈

--- app/src/main/java/com/androidapp/tobeacontinue/etc/Settings.java
+++ app/src/main/java/com/androidapp/tobeacontinue/etc/Settings.java
@@ -1,8 +1,10 @@
 package com.androidapp.tobeacontinue.etc;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 import androidx.annotation.Nullable;
 
@@ -18,11 +20,44 @@ public class Settings extends PreferenceActivity {
                 new MyPreferenceFragment()).commit();
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment{
+    public static class MyPreferenceFragment extends PreferenceFragment {
+
+        SharedPreferences pref;
+
         @Override
         public void onCreate(@Nullable Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preference);
+
+            pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
         }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+
+            pref.registerOnSharedPreferenceChangeListener(listener);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+
+            pref.unregisterOnSharedPreferenceChangeListener(listener);
+
+        }
+
+        SharedPreferences.OnSharedPreferenceChangeListener listener= new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                if(key.equals("message")){
+                    boolean b= pref.getBoolean("message", false);
+
+                }else if(key.equals("vibrate")){
+
+                }
+            }
+        };
     }
 }
